11c7a408100fc657a9fa307cfe2e931917936f6a
Jason Palacios <jason.palacios@utexas.edu>
2018-05-09 20:48:12 -0500

    Edit step ladder dynamic programming problem and unit tests

---
+++ InterviewQuestions.Tests/EditStepLadderTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace InterviewQuestions.Tests
+{
+    [TestClass]
+    public class EditStepLadderTests
+    {
+        private EditStepLadder Ladder { get; } = new EditStepLadder();
+
+        [TestMethod]
+        public void ShouldReturnMaxEditSteps5()
+        { 
+            var words = new List<string>()
+            {
+                "cat",
+                "dig",
+                "dog",
+                "fig",
+                "fin",
+                "fine",
+                "fog",
+                "log",
+                "wine"
+            };
+
+            var maxEditSteps = Ladder.ComputeMaxEditSteps(words);
+
+            Assert.AreEqual(maxEditSteps, 5);
+        }
+
+        [TestMethod]
+        public void ShouldHandleNoPossibleEditStepsAtLastWord()
+        {
+            var words = new List<string>()
+            {
+                "cat",
+                "dig",
+                "dog",
+                "fig",
+                "fin",
+                "fine",
+                "fog",
+                "log",
+                "wine",
+                "zebra"
+            };
+
+            var maxEditSteps = Ladder.ComputeMaxEditSteps(words);
+
+            Assert.AreEqual(maxEditSteps, 5);
+        }
+
+        [TestMethod]
+        public void ShouldHandleNoPossibleEditSteps()
+        {
+            var words = new List<string>()
+            {
+                "cat",
+                "dig",
+                "fin",
+                "fog",
+                "wine",
+                "zebra"
+            };
+
+            var maxEditSteps = Ladder.ComputeMaxEditSteps(words);
+
+            Assert.AreEqual(maxEditSteps, 1);
+        }
+    }
+}
---
+++ InterviewQuestions/EditStepLadder.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewQuestions
+{
+    public class EditStepLadder
+    {
+        public int ComputeMaxEditSteps(List<string> words)
+        {
+            var maxCost = 0;
+            var wordCostMap = new Dictionary<string, int>();
+
+            foreach (var word in words)
+            {
+                wordCostMap[word] = 1;
+                maxCost = Math.Max(maxCost, 1);
+
+                for (var i = 0; i < word.Length; ++i)
+                {               
+                    var wordBeforeIndex = word.Substring(0, i);
+                    var wordUpToIndex = $"{wordBeforeIndex}{word[i]}";
+                    var wordAfterIndex = i < word.Length - 1 ? word.Substring(i + 1) : string.Empty;
+                    var removedLetterAtIndex = $"{wordBeforeIndex}{wordAfterIndex}";
+
+                    if (removedLetterAtIndex.CompareTo(word) < 0 
+                        && wordCostMap.TryGetValue(removedLetterAtIndex, out var removalCost))
+                    {
+                        wordCostMap[word] = Math.Max(wordCostMap[word], removalCost + 1);
+                        maxCost = Math.Max(maxCost, removalCost + 1);
+                    }
+
+                    for (var letter = 'a'; letter <= 'z'; ++letter)
+                    {
+                        var addedLetterAfterIndex = $"{wordUpToIndex}{letter}{wordAfterIndex}";
+                        var exchangedLetterAtIndex = $"{wordBeforeIndex}{letter}{wordAfterIndex}";
+
+                        if (addedLetterAfterIndex.CompareTo(word) < 0
+                            && wordCostMap.TryGetValue(addedLetterAfterIndex, out var additionCost))
+                        {
+                            wordCostMap[word] = Math.Max(wordCostMap[word], additionCost + 1);
+                            maxCost = Math.Max(maxCost, additionCost + 1);
+                        }
+
+                        if (exchangedLetterAtIndex.CompareTo(word) < 0 
+                            && wordCostMap.TryGetValue(exchangedLetterAtIndex, out var exchangeCost))
+                        {
+                            wordCostMap[word] = Math.Max(wordCostMap[word], exchangeCost + 1);
+                            maxCost = Math.Max(maxCost, exchangeCost + 1);
+                        }
+                    }
+                }
+            }
+
+            return maxCost;
+        }
+    }
+}
